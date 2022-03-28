0764ec8d7f300e0eec975c81e102b508505c3549
Hiimzomby <59876681+Hiimzomby@users.noreply.github.com>
2022-03-28 18:59:02 -0400

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Hello World!
+
+This is my first project using all 3 base languages for front end!
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("It's Working!!!");
+
+
+var likes = [9, 12, 9];
+var spans = [
+    document.querySelector("#post1"),
+    document.querySelector("#post2"),
+    document.querySelector("#post3")
+];
+
+function like(id) {
+    likes[id]++;
+    spans[id].innerHTML = likes[id] + "like(s)";
+}
